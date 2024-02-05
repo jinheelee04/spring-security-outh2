bf1cb82a714cc87e9bc0ea2724559aa6c8c8510e
jinheelee04 <2pro.jlee@gmail.com>
2024-02-05 21:25:27 +0900

    자동설정에 의한 초기화 과정 이해

--- src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
+++ src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
@@ -3,23 +3,26 @@ package io.security.outh2.springsecurityouth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
-        http.formLogin((formLogin) -> formLogin
-                .loginPage("/login")
-                .defaultSuccessUrl("/articles"))
-                ;
-         http.with(new CustomSecurityConfigurer(), (conf) -> conf
-                        .setFlag(false));
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
+        http.formLogin(withDefaults());
+//         http.with(new CustomSecurityConfigurer(), (conf) -> conf
+//                        .setFlag(false));
 
+        return http.build();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
+        http.httpBasic(withDefaults());
         return http.build();
     }
 }
