030a9ffa5b7be53615969512c22a1e01be49f3b8
jinheelee04 <2pro.jlee@gmail.com>
2024-02-12 21:43:04 +0900

    AuthenticationEntryPoint 이해

--- src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
+++ src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
@@ -1,9 +1,16 @@
 package io.security.outh2.springsecurityouth2;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,15 +21,22 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
         http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+  /*      http.exceptionHandling((exception) -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("custom entrypoint");
+            }
+        }));*/
 //         http.with(new CustomSecurityConfigurer(), (conf) -> conf
 //                        .setFlag(false));
 
         return http.build();
     }
-    @Bean
-    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
-        http.httpBasic(withDefaults());
-        return http.build();
-    }
+//    @Bean
+//    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
+//        http.httpBasic(withDefaults());
+//        return http.build();
+//    }
 }
