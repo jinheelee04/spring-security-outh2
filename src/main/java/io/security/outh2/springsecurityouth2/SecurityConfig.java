ad8491056e89db7ba25690a11146b6f644fbb0b4
jinheelee04 <2pro.jlee@gmail.com>
2024-02-13 13:28:20 +0900

    Http Basic 인증

--- src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
+++ src/main/java/io/security/outh2/springsecurityouth2/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,8 +21,10 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());// 어떠한 요청이라도 인증필요
-        http.formLogin(withDefaults());
+//        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.httpBasic((basic) -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션을 사용하지 않음, 접근할 때마다 인증 받아야함
   /*      http.exceptionHandling((exception) -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
             @Override
             public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
